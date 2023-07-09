6987611e2eff71c53bdd0978b1eaaa15ea61c35b
brunonp_ <brunonicolaspadin@gmail.com>
2023-07-09 11:17:20 -0300

    Store booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Integrations\Hrb\HrbConnector;
 use App\Http\Integrations\Hrb\Requests\GetRooms;
+use App\Http\Integrations\Hrb\Requests\StoreBooking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
@@ -34,7 +36,11 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = Auth::user();
+        $request->merge(['nameGuest' => $user->name, 'emailGuest' => $user->email]);
+        $hrb = new HrbConnector();
+        $hrb->send(new StoreBooking($request));
+        return back();
     }
 
     /**
